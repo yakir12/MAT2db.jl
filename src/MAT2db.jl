cada542327e1a6e36e2b6f6cefce60912c8deaba
Yakir Gagnon <12.yakir@gmail.com>
2020-10-13 13:44:24 +0200

    delim

--- src/MAT2db.jl
+++ src/MAT2db.jl
@@ -43,7 +43,7 @@ end
 
 function loadcsv(file)
     a_csvfile(file)
-    t = CSV.File(file, normalizenames = true, types = csvfile_columns)
+    t = CSV.File(file, normalizenames = true, types = csvfile_columns)#, delim=';')
     a_table(t)
     return t
 end
