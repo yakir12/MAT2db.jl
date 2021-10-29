3365764c00a470dce0ec3118f2d65254a58d8cfe
Yakir Gagnon <12.yakir@gmail.com>
2021-10-29 11:24:33 +0200

    rm htreadsx

--- src/MAT2db.jl
+++ src/MAT2db.jl
@@ -6,7 +6,7 @@ using MAT, SparseArrays, StaticArrays, CSV
 using GLMakie, FFMPEG, ImageMagick
 import GLMakie.@colorant_str
 using OnlineStats
-using ThreadsX
+# using ThreadsX
 
 using CameraCalibrations
 
@@ -40,7 +40,7 @@ function process_csv(csvfile; debug = false, fun = process_run, delim = nothing)
   mkpath(joinpath(path, "quality", "calibrations"))
   mkpath(joinpath(path, "results"))
   p = Progress(length(t2), 1, "Processing runs...")
-  tracks = ThreadsX.map(enumerate(t2)) do (i, x)
+  tracks = map(enumerate(t2)) do (i, x)
     ProgressMeter.next!(p; showvalues = [(:run_number, i), (:csv_row, i + 1)])
     # println(string(i))
     if debug
